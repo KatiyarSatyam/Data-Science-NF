bfaf3a7b60a8a32df75faa1a292ac27cd49ef182
Satyam Katiyar <85728683+KatiyarSatyam@users.noreply.github.com>
2026-06-03 20:55:47 +0530

    Created using Colab

--- Module 3/Classwork/module3.py
+++ Module 3/Classwork/module3.py
@@ -4,8 +4,7 @@
   "metadata": {
     "colab": {
       "provenance": [],
-      "authorship_tag": "ABX9TyOISf23LYnpiOTqkFo3bJUK",
-      "include_colab_link": true
+      "authorship_tag": "ABX9TyOISf23LYnpiOTqkFo3bJUK"
     },
     "kernelspec": {
       "name": "python3",
@@ -16,16 +15,6 @@
     }
   },
   "cells": [
-    {
-      "cell_type": "markdown",
-      "metadata": {
-        "id": "view-in-github",
-        "colab_type": "text"
-      },
-      "source": [
-        "<a href=\"https://colab.research.google.com/github/KatiyarSatyam/Data-Science-NF/blob/main/Module%203/Classwork/module3.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
-      ]
-    },
     {
       "cell_type": "markdown",
       "source": [
